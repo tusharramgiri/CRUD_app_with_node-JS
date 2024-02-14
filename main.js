57f8c1e1cd5948fa023155744a4757cf6159443f
Tushar Ramgiri <104557273+tusharramgiri@users.noreply.github.com>
2024-02-14 14:41:02 +0530

    main.js creation

--- main.js
+++ main.js
@@ -7,6 +7,24 @@ const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+//database connection
+mongoose.connect(process.env.MONGO_URI)
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log("connected to the database!"));
+
+//middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use(session({
+        secret: 'my secret key',
+        saveUninitialised: true,
+        resave: false,
+    })
+);
+
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
